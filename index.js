1838b86c0d015f000a55c6d59f514fd06d7576b2
arzumanov2004 <sd8cxz70x@code.edu.az>
2023-10-24 23:22:55 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//   1-ci sual 
+
+
+// results.filter(result => {
+//     if (result.order.freight < 1) {
+//         console.log(result.order);
+//     }
+// })
+
+
+
+
+// 2-ci sual
+
+
+// console.log(results.filter(order => order.order.customerId === "ERNSH")); 
+
+
+
+// 3-cu sual
+
+
+
+// console.log(results.sort((a,b) => a.employeeId - b.employeeId));
+
+
+
+// 4-cu sual 
+
+
+
+// results.forEach((order) => {
+//     if (order.orderDetails.length > 3) {
+//       console.log(order.orderDetails.length);
+    
+//     }
+//   });
+
+
+
+
+// 5-ci sual
+
+
+
+// const sifarish = results.find(order => order.order.id === 11075);
+
+// console.log( sifarish);
